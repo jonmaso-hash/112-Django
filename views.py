216cf628d71c03d0d56dc0da6894094bb3dc2d05
jonmaso-hash <jonmaso@gmail.com>
2026-02-28 13:16:19 -0800

    class 3

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-# Create your views here.
-class HomePageView(TemplateView): #OOP - Inheritance (Object Oriented Programming)
-    template_name = "pages/home.html"
-
-class AboutPageView(TemplateView):
-    template_name ="pages/about.html"
